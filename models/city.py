a065a87124736d5ad9b76104d6007113903da93c
Nokuphila Bhembe <nnblb58@gmail.com>
2024-03-05 13:32:39 +0200

    Airbnb_clone_v2

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship(
+        'Place',
+        cascade='all, delete-orphan',
+        backref='cities'
+    )
---
+++ models/place.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128))
+    description = Column(String(1024))
+    number_rooms = Column(Integer(), default=0, nullable=False)
+    number_bathrooms = Column(Integer(), default=0, nullable=False)
+    max_guest = Column(Integer(), default=0, nullable=False)
+    price_by_night = Column(Integer(), default=0, nullable=False)
+    latitude = Column(Float())
+    longitude = Column(Float())
+    amenity_ids = []
